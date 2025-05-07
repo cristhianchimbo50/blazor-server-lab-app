2b182c5cf4bb405a63289ccc921390a7570ea472
cristhianchimbo50 <chimbocristhian994@gmail.com>
2025-05-07 01:52:17 -0500

    Realizado la configuración de consumo de API desde Blazor Server (GET y POST de Paciente)

---
+++ LabWebAppBlazor/Services/ApiService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Json;
+using LabWebAppBlazor.Models;
+
+namespace LabWebAppBlazor.Services
+{
+    public class ApiService : IApiService
+    {
+        private readonly HttpClient _http;
+
+        public ApiService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<IEnumerable<PacienteDto>> GetPacientesAsync()
+        {
+            return await _http.GetFromJsonAsync<IEnumerable<PacienteDto>>("pacientes");
+        }
+
+        public async Task<HttpResponseMessage> CrearPacienteAsync(PacienteDto paciente)
+        {
+            var response = await _http.PostAsJsonAsync("pacientes", paciente);
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"STATUS: {response.StatusCode}");
+            Console.WriteLine($"BODY: {content}");
+            return response;
+        }
+    }
+}

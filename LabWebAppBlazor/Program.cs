2cb8c264ae5e87db3ebe8012f519eff0515d1711
cristhianchimbo50 <chimbocristhian994@gmail.com>
2025-07-02 08:55:47 -0500

    Cambio api

--- LabWebAppBlazor/Program.cs
+++ LabWebAppBlazor/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddHttpClient("Api", client =>
 {
-    client.BaseAddress = new Uri("http://localhost:5077/api/");
+    client.BaseAddress = new Uri("https://apirest-lab-app.onrender.com/api/");
 });
 
 builder.Services.AddScoped<ProtectedSessionStorage>();
